c805a8faf9f83bd7dc842e5970dd7a0fe6f3f262
JojiK85 <kandulajoji@gmail.com>
2025-12-17 11:52:55 +0530

    Update pravaah.js

--- api/pravaah.js
+++ api/pravaah.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
     const qs = new URLSearchParams(req.query).toString();
 
     const url =
-      "https://script.google.com/macros/s/AKfycbxA4kUMYAaDE-HP07EWZa5Nm8pWYqUvUO-WzxkUoR3jXRjqPOvJ9Rv91P8glYNLJfMjrw/exec?" +
+      "https://script.google.com/macros/s/AKfycbyAiWPJSE7Qm3qPyL-vZTlBR07xlhqzSRxfu-eeXMnXowOE8GXc1AZsLGW1ZOwyP7kjow/exec?" +
       qs;
 
     const r = await fetch(url);
