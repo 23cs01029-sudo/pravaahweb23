37473d7ef18858f9918a1412b921e5180e7254e2
JojiK85 <kandulajoji@gmail.com>
2025-12-18 11:18:12 +0530

    Update pravaah.js

--- api/pravaah.js
+++ api/pravaah.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   try {
     const GAS_URL =
-      "https://script.google.com/macros/s/AKfycbw-ewwk4WzhvXyHmS1bK3_iAHYHufWfyQCOLfuzWO7GkoRqHOWb7hxhBQZe3KbJcPrEUw/exec";
+      "https://script.google.com/macros/s/AKfycbzMGxDeQEXl9eM2GQ_VmGcJT5bWZvldhQ1E5QlSX1mTc5TovSzLpwtEzSEpxu8Gd81e0Q/exec";
 
     let url = GAS_URL;
 
