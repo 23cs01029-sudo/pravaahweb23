2ee6bf286cf0c5cf655cb33c22a329d614f4cdbb
GaneshNaik2006 <ganeshiitbombay225@gmail.com>
2026-01-24 23:40:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const feedsByDate = { 
      "2026-01-27": [
-      { img: "pravah-logo.png", name: "9 days to go", text: "Somethings cooking", time: "soon" },
+      { img: "pravah-logo.png", name: "9 days to go", text: "Somethings cooking", time: "soon"  },
     ],
      "2026-01-28": [
       { img: "Pravaah logo.png", name: "8 days to go", text: "Somethings cooking", time: "soon" },
@@ -37,27 +37,27 @@ document.addEventListener("DOMContentLoaded", () => {
       { img: "Pravaah logo.png", name: "1 day to go", text: "Somethings cooking", time: "soon" },
     ],
     "2026-02-05": [
-      { img: "Inaugural.jpg", name: "Pravaah", text: "Opening Cermony", time: "6:30 PM" },
+      { img: "Inaugural.jpg", name: "Pravaah", text: "Opening Cermony", time: "6:30 PM" ,locationLink:"https://maps.app.goo.gl/BDz4WQp2ZXHVktaR9"},
     ],
   "2026-02-06": [
-  { img: "Guest Talk.JPG", name: "Guest Talk – 1", text: "Inspiring expert insights", time: "12:00 PM" },
-  { img: "fashionshow_gallery.jpg", name: "Fashion Show", text: "Style meets confidence", time: "2:00 PM" },
-  { img: "ML HACKTHON.jpg", name: "Data Science Hackathon", text: "Code,Analyze,Innovate.", time: "9:00 AM" },
-  { img: "Film Quiz_events.JPG", name: "General Quiz", text: "Test your knowledge", time: "9:00 AM" },
+  { img: "Guest Talk.JPG", name: "Guest Talk – 1", text: "Inspiring expert insights", time: "12:00 PM" ,locationLink:"https://maps.app.goo.gl/6xiczMEfwDUEBZQ28" },
+  { img: "fashionshow_gallery.jpg", name: "Fashion Show", text: "Style meets confidence", time: "2:00 PM",locationLink:"https://maps.app.goo.gl/BDz4WQp2ZXHVktaR9" },
+  { img: "ML HACKTHON.jpg", name: "Data Science Hackathon", text: "Code,Analyze,Innovate.", time: "9:00 AM" ,locationLink:"https://maps.app.goo.gl/n6YXxLQmicNHihqi7"},
+  { img: "Film Quiz_events.JPG", name: "General Quiz", text: "Test your knowledge", time: "9:00 AM",locationLink:"https://maps.app.goo.gl/6xiczMEfwDUEBZQ28" },
 
-  { img: "Marcatus.JPG", name: "Marcatus – Round 1", text: "Business minds battle", time: "10:00 PM" },
-  { img: "informal night.JPG", name: "Informal Night", text: "Fun without filters", time: "6:00 PM" },
+  { img: "Marcatus.JPG", name: "Marcatus – Round 1", text: "Business minds battle", time: "10:00 PM" ,locationLink:"https://maps.app.goo.gl/6xiczMEfwDUEBZQ28" },
+  { img: "informal night.JPG", name: "Informal Night", text: "Fun without filters", time: "6:00 PM",locationLink:"https://maps.app.goo.gl/jDQyDv2pJuNpothj7" },
 
-  { img: "Marcatus.JPG", name: "Marcatus – Round 2", text: "Business minds battle", time: "2:30 PM" },
-  { img: "startup expo event.jpg", name: "Startup Expo", text: "Ideas meet investors", time: "2:00 PM" },
-  { img: "face painting events.webp", name: "Face Painting", text: "Colors come alive", time: "6:00 PM" },
+  { img: "Marcatus.JPG", name: "Marcatus – Round 2", text: "Business minds battle", time: "2:30 PM" ,locationLink:"https://maps.app.goo.gl/6xiczMEfwDUEBZQ28"},
+  { img: "startup expo event.jpg", name: "Startup Expo", text: "Ideas meet investors", time: "2:00 PM",locationLink:"https://maps.app.goo.gl/6xiczMEfwDUEBZQ28" },
+  { img: "face painting events.webp", name: "Face Painting", text: "Colors come alive", time: "6:00 PM" ,locationLink:"https://maps.app.goo.gl/jDQyDv2pJuNpothj7" },
 
   
-  { img: "robo soccor.JPG", name: "Robo Soccer", text: "Robots,Goals,Glory", time: "2:00 PM" },
+  { img: "robo soccor.JPG", name: "Robo Soccer", text: "Robots,Goals,Glory", time: "2:00 PM",locationLink:"https://maps.app.goo.gl/jDQyDv2pJuNpothj7" },
   { img: "51hr flimmaking events.avif", name: "51-Hour Short Film – Start", text: "Create,Shoot,Edit.", time: "6:00 PM" },
 
   
-  { img: "abhinay event.jpeg", name: "Abhinay", text: "Act,Express,Perform.", time: "2:00 PM" }
+  { img: "abhinay event.jpeg", name: "Abhinay", text: "Act,Express,Perform.", time: "2:00 PM" ,locationLink:"https://maps.app.goo.gl/h175EDFX6i3GMWbt9" }
   
 ],
 
@@ -65,45 +65,45 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // ================= DAY 2 (Feb 7) =================
 "2026-02-07": [
-  { img: "Guest Talk.JPG", name: "Guest Talk – 2", text: "Act,Express,Perform.", time: "12:00 PM" },
+  { img: "Guest Talk.JPG", name: "Guest Talk – 2", text: "Act,Express,Perform.", time: "12:00 PM",locationLink:"https://maps.app.goo.gl/6xiczMEfwDUEBZQ28" },
   { img: "51hr flimmaking events.avif", name: "51-Hour Short Film", text: "Create,Shoot,Edit.", time: "8:00 AM" },
-  { img: "ML HACKTHON.jpg", name: "Web Hackathon", text: "Build,Deploy,Innovate", time: "8:00 AM" },
+  { img: "ML HACKTHON.jpg", name: "Web Hackathon", text: "Build,Deploy,Innovate", time: "8:00 AM",locationLink:"https://maps.app.goo.gl/n6YXxLQmicNHihqi7" },
 
-  { img: "data sceince event.jpg", name: "Science & Technology Quiz", text: "Where minds compete", time: "9:00 AM" },
-  { img: "B plan event.jpg", name: "B-Plan", text: "Pitch your startup", time: "2:00 PM" },
+  { img: "data sceince event.jpg", name: "Science & Technology Quiz", text: "Where minds compete", time: "9:00 AM",locationLink:"https://maps.app.goo.gl/6xiczMEfwDUEBZQ28" },
+  { img: "B plan event.jpg", name: "B-Plan", text: "Pitch your startup", time: "2:00 PM",locationLink:"https://maps.app.goo.gl/6xiczMEfwDUEBZQ28" },
 
-  { img: "Enigma_gallery.JPG", name: "Enigma – Round 1", text: "Crack the mystery", time: "10:00 AM" },
-  { img: "Enigma_events.JPG", name: "Enigma – Round 2", text: "Crack the mystery", time: "2:00 PM" },
+  { img: "Enigma_gallery.JPG", name: "Enigma – Round 1", text: "Crack the mystery", time: "10:00 AM" ,locationLink:"https://maps.app.goo.gl/6xiczMEfwDUEBZQ28"},
+  { img: "Enigma_events.JPG", name: "Enigma – Round 2", text: "Crack the mystery", time: "2:00 PM",locationLink:"https://maps.app.goo.gl/6xiczMEfwDUEBZQ28" },
 
-  { img: "Innovation Expo_gallery.JPG", name: "Innovation Expo", text: "Future on display", time: "2:00 PM" },
+  { img: "Innovation Expo_gallery.JPG", name: "Innovation Expo", text: "Future on display", time: "2:00 PM",locationLink:"https://maps.app.goo.gl/6xiczMEfwDUEBZQ28" },
   
 
-  { img: "blastoff event.jpg", name: "Blast Off", text: "Ignite,Launch,Celebrate.", time: "10:00 AM" },
-  { img: "RoboRace.jpg", name: "Robo Race", text: "Speed,Tech,Thrill.", time: "2:00 PM" },
+  { img: "blastoff event.jpg", name: "Blast Off", text: "Ignite,Launch,Celebrate.", time: "10:00 AM",locationLink:"https://maps.app.goo.gl/RUXkuHvS72VWrdku5" },
+  { img: "RoboRace.jpg", name: "Robo Race", text: "Speed,Tech,Thrill.", time: "2:00 PM",locationLink:"https://maps.app.goo.gl/jDQyDv2pJuNpothj7"  },
 
-  { img: "monoct events.jpeg", name: "Monoact", text: "Solo story performance",time: "8:00 AM" },
-  { img: "Tamasha_gallery.jpg", name: "Tamasha", text: "Drama and entertainment", time: "3:00 PM" },
-  { img: "Pravaah logo.png", name: "Comedy Night", text: "Laughs all-around", time: "6:30 PM" }
+  { img: "monoct events.jpeg", name: "Monoact", text: "Solo story performance",time: "8:00 AM",locationLink:"https://maps.app.goo.gl/Kj77m4LAc68SfaXf8" },
+  { img: "Tamasha_gallery.jpg", name: "Tamasha", text: "Drama and entertainment", time: "3:00 PM" ,locationLink:"https://maps.app.goo.gl/Kj77m4LAc68SfaXf8"},
+  { img: "Pravaah logo.png", name: "Comedy Night", text: "Laughs all-around", time: "6:30 PM" ,locationLink:"https://maps.app.goo.gl/jDQyDv2pJuNpothj7" }
 ],
 
 
 
   // ================= DAY 3 (Feb 8) =================
  "2026-02-08": [
-  { img: "Guest Talk.JPG", name: "Guest Talk – 3", text: "Inspiring expert insights", time: "12:00 PM" },
+  { img: "Guest Talk.JPG", name: "Guest Talk – 3", text: "Inspiring expert insights", time: "12:00 PM",,locationLink:"https://maps.app.goo.gl/6xiczMEfwDUEBZQ28" },
   { img: "51hr flimmaking events.avif", name: "51-Hour Short Film", text: "Create,Shoot,Edit.", time: "8:00 AM" },
-  { img: "edusphere.png", name: "Edusphere", text: "Learn,Explore,Grow.", time: "10:00 AM" },
+  { img: "edusphere.png", name: "Edusphere", text: "Learn,Explore,Grow.", time: "10:00 AM",,locationLink:"https://maps.app.goo.gl/6xiczMEfwDUEBZQ28" },
 
-  { img: "ipl quiz_events (1).png", name: "IPL Auction – Round 1", text: "Bid,Strategize,Win.", time: "10:00 AM" },
-  { img: "ipl quiz_events (1).png", name: "IPL Auction – Round 2", text: "Bid,Strategize,Win.", time: "2:00 PM" },
+  { img: "ipl quiz_events (1).png", name: "IPL Auction – Round 1", text: "Bid,Strategize,Win.", time: "10:00 AM" ,locationLink:"https://maps.app.goo.gl/6xiczMEfwDUEBZQ28"},
+  { img: "ipl quiz_events (1).png", name: "IPL Auction – Round 2", text: "Bid,Strategize,Win.", time: "2:00 PM" ,locationLink:"https://maps.app.goo.gl/6xiczMEfwDUEBZQ28"},
 
-  { img: "SoloDance_events.jpg", name: "Solo Dance", text: "Express through movement", time: "9:30 AM" },
-  { img: "group Dance_events.JPG", name: "Group Dance", text: "Unity in motion", time: "1:30 PM" },
-  { img: "trekkon event.jpg", name: "Trekkon", text: "Robots on track", time: "10:00 PM" },
-  { img: "StreetDanceBattle_gallery.jpg", name: "Street Battle", text: "Dance,Clash,Dominate.", time: "4:00 PM" },
+  { img: "SoloDance_events.jpg", name: "Solo Dance", text: "Express through movement", time: "9:30 AM" ,locationLink:"https://maps.app.goo.gl/nxKumvFAEe4KybS26"},
+  { img: "group Dance_events.JPG", name: "Group Dance", text: "Unity in motion", time: "1:30 PM",locationLink:"https://maps.app.goo.gl/nxKumvFAEe4KybS26" },
+  { img: "trekkon event.jpg", name: "Trekkon", text: "Robots on track", time: "10:00 PM",locationLink:"https://maps.app.goo.gl/6xiczMEfwDUEBZQ28" },
+  { img: "StreetDanceBattle_gallery.jpg", name: "Street Battle", text: "Dance,Clash,Dominate.", time: "4:00 PM" ,locationLink:"https://maps.app.goo.gl/Kj77m4LAc68SfaXf8"},
 
-  { img: "StarNight_events.JPG", name: "Star Night", text: "Stars,Lights,Vibes.", time: "7:00 PM" },
-  { img: "Screenshot 2026-01-24 010231.png", name: "DJ Night", text: "Beats,Bass,Blast.", time: "7:00 PM" }
+  { img: "StarNight_events.JPG", name: "Star Night", text: "Stars,Lights,Vibes.", time: "7:00 PM" ,locationLink:"https://maps.app.goo.gl/6mLvvmnVb1UTvo2G7"},
+  { img: "Screenshot 2026-01-24 010231.png", name: "DJ Night", text: "Beats,Bass,Blast.", time: "7:00 PM",locationLink:"https://maps.app.goo.gl/6mLvvmnVb1UTvo2G7" }
 ]
 
 
@@ -458,6 +458,7 @@ window.addEventListener("blur", () => {
 window.addEventListener("beforeunload", () => {
   stopYouTubeVideo();
 });
+
 
 
 
